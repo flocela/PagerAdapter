554311d8a38f25b93de08cbbb4348c8c96ab4201
Flo B Jones <flocela@gmail.com>
2017-01-06 16:43:39 -0600

    test still not working

--- app/src/androidTest/java/flobee/pageradapterex/StraitLineTest.java
+++ app/src/androidTest/java/flobee/pageradapterex/StraitLineTest.java
@@ -1,14 +1,20 @@
 package flobee.pageradapterex;
 
 
+import android.app.Activity;
+import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.view.ViewPager;
 
+import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.registerIdlingResources;
+import static android.support.test.espresso.Espresso.unregisterIdlingResources;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
@@ -16,11 +22,14 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.core.AllOf.allOf;
 import static org.hamcrest.Matchers.not;
+import static org.hamcrest.core.AllOf.allOf;
 
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class StraitLineTest {
+
+  private ViewPagerIdlingResource idlingResource;
 
   private static final String shmi_s   = "Shmi Skywalker";
   private static final String anakin_s = "Anakin Skywalker";
@@ -28,33 +37,35 @@ public class StraitLineTest {
   private static final String jacen_s  = "Jacen Solo";
   private static final String allana_s = "Allana Solo";
 
+  // launch flag should be false so that it is not lazily instantiated. Instead I
+  // launch the activity in startActivity().
   @Rule
-  public ActivityTestRule mActivityRule = new ActivityTestRule(MainActivity.class);
+  public ActivityTestRule<MainActivity> mActivityRule =
+    new ActivityTestRule<MainActivity>(MainActivity.class, true, false);
+
+  @After
+  public void tearDownIdlingResource () {
+    unregisterIdlingResources(idlingResource);
+  }
 
   @Test
   public void firstSwipe () {
+    Activity activity = startActivity();
+
+    idlingResource = new ViewPagerIdlingResource((ViewPager)activity.
+      findViewById(R.id.view_pager), "VPIR_0");
+    registerIdlingResources(idlingResource);
+
     onView(isRoot()).perform(swipeLeft());
-    waitForViewPagerResponse(1000);
+
     onView(allOf(withId(R.id.character_name),withText(anakin_s))).
-      check(matches(isCompletelyDisplayed()));
-
+      check(matches(isDisplayed()));
     onView(allOf(withId(R.id.character_name),withText(shmi_s))).
-      check(matches(not(isDisplayed())));
-   // onView(withId(R.id.character_pict)).
-    //  check(matches(DrawableMatcher.withCharacterName("Anakin Skywalker")));
-
-   // onView(isRoot()).perform(swipeLeft()).
-   //   check(matches(allOf(withText(leia_o),isDisplayed())));
-   // onView(withId(R.id.character_pict)).
-   //   check(matches(DrawableMatcher.withCharacterName("Leia Organa")));
+      check(matches(not(isCompletelyDisplayed())));
   }
 
-  public static void waitForViewPagerResponse(long millis) {
-    final long startTime = System.currentTimeMillis();
-    final long endTime = startTime + millis;
-    do {
-    }
-    while (System.currentTimeMillis() < endTime);
+  private MainActivity startActivity() {
+    return mActivityRule.launchActivity(null);
   }
 
 }
